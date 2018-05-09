45747689625c3f818b7544df38c52c07701aad37
nazchris <christian.naziliwanlongo@student.unamur.be>
2018-05-09 23:22:33 +0200

    Ajout de la classe qui gère le type

--- src/main/java/be/unamur/info/b314/compiler/main/Type.java
+++ src/main/java/be/unamur/info/b314/compiler/main/Type.java
@@ -1,4 +1,26 @@
 package be.unamur.info.b314.compiler.main;
 
+/**
+ * Class for the type
+ */
 public class Type {
+
+    private String name;
+
+    /**
+     * Constructor for type class
+     * @param name of the new type
+     */
+
+    public Type (String name) {
+        this.name = name;
+    }
+
+    /**
+     * @return name of the calling type
+     */
+
+    public String getName() {
+        return this.name;
+    }
 }
