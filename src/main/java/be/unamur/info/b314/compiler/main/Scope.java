01e73399b8674f507f8dcfdeebbe51468a643f07
nazchris <christian.naziliwanlongo@student.unamur.be>
2018-05-09 23:21:42 +0200

    Ajout de la classe Scope

--- src/main/java/be/unamur/info/b314/compiler/main/Scope.java
+++ src/main/java/be/unamur/info/b314/compiler/main/Scope.java
@@ -1,4 +1,17 @@
 package be.unamur.info.b314.compiler.main;
 
-public class Scope {
+import org.mozilla.javascript.ast.Symbol;
+import be.unamur.info.b314.compiler.main.Exception.NonValideLocalVarDeclException;
+import be.unamur.info.b314.compiler.main.Exception.TwoSameGlobalVarDeclException;
+
+public interface Scope {
+
+    String getScopeName();
+    Scope getEnclosingScope();
+
+    void define (Symbol symbol) throws NonValideLocalVarDeclException, TwoSameGlobalVarDeclException;
+
+    Symbol relsolve (String name);
+
+    int getVarCount();
 }
