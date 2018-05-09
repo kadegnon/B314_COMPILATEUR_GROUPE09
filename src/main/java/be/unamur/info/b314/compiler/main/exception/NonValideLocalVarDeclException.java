b39eacb3fe1f52e6e3d54ce3fc7e1b47ccc46d4b
nazchris <christian.naziliwanlongo@student.unamur.be>
2018-05-09 23:24:19 +0200

    Ajout du dossier des exception

--- src/main/java/be/unamur/info/b314/compiler/main/exception/NonValideLocalVarDeclException.java
+++ src/main/java/be/unamur/info/b314/compiler/main/exception/NonValideLocalVarDeclException.java
@@ -1,4 +1,14 @@
 package be.unamur.info.b314.compiler.main.exception;
 
-public class NonValideLocalVarDeclException {
+import org.mozilla.javascript.ast.Symbol;
+
+public class NonValideLocalVarDeclException extends RuntimeException {
+
+    private Symbol symbol;
+    private Symbol symbol1;
+
+    public NonValideLocalVarDeclException (Symbol symbol, Symbol symbol1) {
+        this.symbol = symbol;
+        this.symbol1 = symbol1;
+    }
 }
--- src/main/java/be/unamur/info/b314/compiler/main/exception/TwoSameGlobalVarDeclException.java
+++ src/main/java/be/unamur/info/b314/compiler/main/exception/TwoSameGlobalVarDeclException.java
@@ -1,4 +1,10 @@
 package be.unamur.info.b314.compiler.main.exception;
 
-public class TwoSameGlobalVarDeclException {
+public class TwoSameGlobalVarDeclException extends RuntimeException {
+
+    private static final String msg = "Variable already existed";
+
+    public TwoSameGlobalVarDeclException (String name) {
+        super(msg + name);
+    }
 }
