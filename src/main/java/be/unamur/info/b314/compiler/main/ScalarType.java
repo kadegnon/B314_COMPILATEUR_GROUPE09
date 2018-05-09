c912d3e16fc32f6ae8c6b37c25bb1043cc7fb3c7
nazchris <christian.naziliwanlongo@student.unamur.be>
2018-05-09 23:20:42 +0200

    Ajout de la classe pour le scalar type

--- src/main/java/be/unamur/info/b314/compiler/main/ScalarType.java
+++ src/main/java/be/unamur/info/b314/compiler/main/ScalarType.java
@@ -1,4 +1,12 @@
 package be.unamur.info.b314.compiler.main;
 
-public class ScalarType {
+public class ScalarType extends Type {
+    /**
+     * Constructor for type class
+     *
+     * @param name of the new type
+     */
+    public ScalarType(String name) {
+        super(name);
+    }
 }
